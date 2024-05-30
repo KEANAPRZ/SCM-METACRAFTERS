0a09dc48241ffeb9f210fb00bb1390784efab2f9
KEANA ALIZA C. PEREZ <137427255+KEANAPRZ@users.noreply.github.com>
2024-05-30 22:57:09 +0800

    Update Assessment.sol

--- AVAX ETH MODULE 2/contracts/Assessment.sol
+++ AVAX ETH MODULE 2/contracts/Assessment.sol
@@ -1,60 +1,25 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-//import "hardhat/console.sol";
+pragma solidity ^0.8.0;
 
 contract Assessment {
-    address payable public owner;
-    uint256 public balance;
+  struct Book {
+    string title;
+    string author;
+  }
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+  mapping(uint => Book) public books;
+  uint public bookCount;
 
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
-    }
+  constructor() {
+    bookCount = 0;
+  }
 
-    function getBalance() public view returns(uint256){
-        return balance;
-    }
+  function addBook(string memory title, string memory author) public {
+    books[bookCount] = Book(title, author);
+    bookCount++;
+  }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
-    }
-
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
-
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
-    }
+  function getBook(uint bookId) public view returns (string memory, string memory) {
+    require(bookId < bookCount, "Book ID out of range");
+    return (books[bookId].title, books[bookId].author);
+  }
 }
