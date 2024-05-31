a9ab7ea21c1ab3bdc918a83aa0446236d8ac23af
KEANA ALIZA C. PEREZ <137427255+KEANAPRZ@users.noreply.github.com>
2024-06-01 04:54:49 +0800

    Update Assessment.sol

--- AVAX ETH MODULE 2/contracts/Assessment.sol
+++ AVAX ETH MODULE 2/contracts/Assessment.sol
@@ -1,25 +1,58 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
 contract Assessment {
-  struct Book {
-    string title;
-    string author;
-  }
+    address payable public owner;
+    uint256 public balance;
+    uint public bookCount;
 
-  mapping(uint => Book) public books;
-  uint public bookCount;
+    struct Book {
+        string title;
+        string author;
+    }
 
-  constructor() {
-    bookCount = 0;
-  }
+    mapping(uint => Book) public books;
 
-  function addBook(string memory title, string memory author) public {
-    books[bookCount] = Book(title, author);
-    bookCount++;
-  }
+    event Deposit(uint256 amount);
+    event Withdraw(uint256 amount);
 
-  function getBook(uint bookId) public view returns (string memory, string memory) {
-    require(bookId < bookCount, "Book ID out of range");
-    return (books[bookId].title, books[bookId].author);
-  }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
+
+    constructor(uint initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
+        bookCount = 0;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return balance;
+    }
+
+    function addBook(string memory title, string memory author) public {
+        books[bookCount] = Book(title, author);
+        bookCount++;
+    }
+
+    function getBook(uint bookId) public view returns (string memory, string memory) {
+        require(bookId < bookCount, "Book ID out of range");
+        return (books[bookId].title, books[bookId].author);
+    }
+
+    function deposit(uint256 _amount) public payable onlyOwner {
+        uint256 _previousBalance = balance;
+        balance += _amount;
+        assert(balance == _previousBalance + _amount);
+        emit Deposit(_amount);
+    }
+
+    function withdraw(uint256 _withdrawAmount) public onlyOwner {
+        uint256 _previousBalance = balance;
+        require(balance >= _withdrawAmount, "Insufficient balance");
+        balance -= _withdrawAmount;
+        assert(balance == (_previousBalance - _withdrawAmount));
+        emit Withdraw(_withdrawAmount);
+    }
 }
